3b58e31921f214a8971696d0a2fa4efd75d57ca1
Guany <1084072690@qq.com>
2023-11-24 09:22:43 +0800

    chore: update

--- src/components/TNavBar.vue
+++ src/components/TNavBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav flex="~ gap-4" justify-center text-xl>
+  <nav flex justify-center gap-2 text-xl>
     <button icon-btn title="切换深色模式" @click="toggleDark">
       <div i-carbon-sun dark:i-carbon-moon />
     </button>
 
-    <a icon-btn rel="noreferrer" href="https://github.com/tlyboy/vue-starter" target="_blank" title="GitHub">
+    <a icon-btn rel="noreferrer" href="https://github.com/tlyboy/vant-vue-starter" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
   </nav>
